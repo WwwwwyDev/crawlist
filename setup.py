a98171953b2df071cf79882b9b19856e116756b7
WwyDev <773323518@qq.com>
2024-04-19 14:07:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
