f04e1e3fc82ca0ca2d99d63d92eea1be58b8c23e
wuwenyi <wwwwwydev@gmail.com>
2024-05-15 15:34:03 +0800

    version 0.1.0
    fix bugs

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/WwwwwyDev/crawlist'
 EMAIL = 'wwy20001014@foxmail.com'
 AUTHOR = 'WwyDev'
 REQUIRES_PYTHON = '>=3.10.0'
-VERSION = '0.0.9'
+VERSION = '0.1.0'
 # What packages are required for this module to be executed?
 REQUIRED = [
     'parsel', 'selenium>=4.0.0', 'cssselect', 'lxml', 'requests', 'webdriver-manager'
